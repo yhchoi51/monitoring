f3bf3bb2b3867460393e8a8855683158d5feb0b3
freelife <>
2022-09-15 11:26:36 +0900

    Initial commit

---
+++ src/test/java/kr/pe/freelife/monitoring/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.freelife.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
